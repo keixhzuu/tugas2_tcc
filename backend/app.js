7fba4f2ac7324c05eecc8b9dcb7d67c124f638b1
keixhzuu <melania.ints18@gmail.com>
2026-05-05 01:59:52 +0700

    api2.rest

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,24 @@
-
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/database');
 const notesRoutes = require('./routes/notesRoute');
 
-
 const app = express();
 
-
 app.use(cors());
-
-
 app.use(express.json());
 
 // Routes
 app.use('/notes', notesRoutes); 
 
+// --- TAMBAHKAN KODE INI ---
+app.get('/', (req, res) => {
+    res.send('Welcome to  Backend API! Go to /notes to see the data.');
+});
+// --------------------------
+
 // Port
 const port = process.env.PORT || 3000;
-
 
 sequelize.sync()
   .then(() => {
